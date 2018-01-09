9217e09b6ba7f177647b59d9e71540ee54b5448f
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-01-09 17:45:34 +0530

    Create zero.c

---
+++ zero.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	printf("enter the no.");
+	scanf("%d",&n);
+	if(n>0)
+	printf("no is positive");
+	elseif(n<0)
+	printf("no is negative");
+	else
+	printf("no is zero");
+	
+	return 0;
+}
