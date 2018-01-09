63b8cf445610e48ffe9dab23c4dfe1cf95d75061
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-01-09 18:03:07 +0530

    Create ABC.c

---
+++ ABC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	char a;
+	printf("enter the letter");
+	scanf("%c",&a);
+	if(c>='a' && c<='z')
+	printf("%c albhabet",a);
+	else
+	printf("%c not albhabet",a);
+	return 0;
+}
