4b31ba6632660e4f7ab15b39a55941270c50e2c8
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-01-19 16:56:09 +0530

    Create 5multiples.c

---
+++ 5multiples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int n,i,l;
+	printf("enter the no");
+	scanf("%d",&n);
+	printf("enter the limit");
+	scanf("%d",&l);
+	for(i=1;i<=l;i++)
+	if(i%n==0)
+	printf("%d",i);
+	
+
+	return 0;
+}
