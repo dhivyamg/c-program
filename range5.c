9b3674d11d8689acb21d68ee6abfd2ed692e0185
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-01-23 18:03:54 +0530

    Create range5.c

---
+++ range5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+ int a;
+ printf("enter the no.");
+ scanf("%d",&a);
+ if(a<10)
+ {
+ 	printf("yse with in the range %d");
+ }
+ else
+ {
+ 	printf("not with in the range");
+ }
+	return 0;
+}
