5387832d836e3597932d187d6122815667d03bec
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-01-28 13:43:21 +0530

    Create 38swap.c

---
+++ 38swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int c,d;
+    printf("enter two no.");
+    scanf("%d%d",&c,&d);
+     c=c^d;
+     d=c^d;
+     c=c^d;
+     printf("%d%d",c,d);
+	return 0;
+}
