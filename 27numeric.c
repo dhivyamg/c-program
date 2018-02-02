7c1e5e6e3a3a1468ab3e34178584f7c4bfea7aa3
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-02-02 15:41:23 +0530

    Create 27numeric.c

---
+++ 27numeric.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+	char ch;
+	printf("enter the letter");
+	scanf("%c",&ch);
+	if(ch>='a' && ch<='z')
+	printf("%c not numeric",ch);
+	else
+	printf("%c numeric",ch);
+	return 0;
+}
