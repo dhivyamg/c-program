a4b1ad784a70b82b9b8c78d7924da6082ccd7671
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-02-02 16:13:29 +0530

    Create 30time.c

---
+++ 30time.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b,c,d,e,f;
+	printf("enter the  1st time");
+	scanf("%d %d",&a,&b);
+	printf("enter the 2nd time");
+	scanf("%d %d",&c,&d);
+	e=a-c;
+	f=b-d;
+	printf("%d %d",e,f);
+	
+	return 0;
+}
