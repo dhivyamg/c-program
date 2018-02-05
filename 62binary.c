665d9fab17582dc9ec38fb0cf3f98c8062de7b3b
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-02-05 16:04:03 +0530

    Create 62binary.c

---
+++ 62binary.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[67];
+	int i;
+	printf("enter the string");
+	scanf("%s",a);
+	if(a[i]=='1'||a[i]=='0')
+	{
+		printf("yes it is binary");
+		
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
