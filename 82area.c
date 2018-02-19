9617b28cfec4890951b5cc4252f59982a2df4abf
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-02-19 16:07:24 +0530

    Create 82area.c

---
+++ 82area.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+      float a,b,c;
+      printf("enter the length and breath\n");
+      scanf("%f%f",&a,&b);
+      c=a*b;
+      printf("area is %0.5f",c);
+	return 0;
+}
